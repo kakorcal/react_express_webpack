9f5160d71404a45348ef3f9bd845f75b6ca03f6a
Kenneth Korcal <kakorcal@gmail.com>
2016-08-05 16:46:38 -0700

    building routes for users

--- server/api/users.js
+++ server/api/users.js
@@ -3,23 +3,33 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', (req, res)=>{
-
+  knex('users').then(users=>{
+    res.send(users);
+  });
 });
 
 router.post('/', (req, res)=>{
-
+  knex('users').insert(req.body.user).then(()=>{
+    
+  });
 });
 
 router.get('/:id', (req, res)=>{
-
+  knex('users').where('id', +req.params.id).then(user=>{
+    res.send(user);
+  });
 });
 
 router.put('/:id', (req, res)=>{
+  knex('users').where('id', +req.params.id).update(req.body.user).then(()=>{
 
+  });
 });
 
 router.delete('/:id', (req, res)=>{
+  knex('users').where('id', +req.params.id).del().then(()=>{
 
+  });
 });
 
 module.exports = router;
