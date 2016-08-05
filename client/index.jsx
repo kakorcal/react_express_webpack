d13e4a5fd0cbbf1613669b8b73ab7cd4123f58e4
Kenneth Korcal <kakorcal@gmail.com>
2016-08-05 16:13:58 -0700

    setting up api

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './base.scss';
+require('./base.scss');
 
 const App = React.createClass({
   render(){
--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
-// const routes = require('./api/index');
+const routes = require('./api/index');
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -27,6 +27,8 @@ app.use(express.static(path.join(__dirname, '../build')));
 app.get('/', (req, res)=>{
   res.sendFile(path.join(__dirname, '../build/index.html'))
 });
+
+app.use('/api/users', routes.users);
 
 // app.use((req, res, next)=>{
 //   const err = new Error('Oops!!');
