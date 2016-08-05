59b600e3b9adf87296448a30666a525ce62c426d
Kenneth Korcal <kakorcal@gmail.com>
2016-08-05 08:04:58 -0700

    redoing configs

--- build/app.6b0c407aef849210f220.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0,2],[function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{"default":e}}var u=l(1),a=n(u),r=l(31),d=n(r),c=a["default"].createClass({displayName:"App",render:function(){return a["default"].createElement("div",null,a["default"].createElement("h1",null,"React Address Book"))}});d["default"].render(a["default"].createElement(c,null),document.getElementById("root"))}]);
-//# sourceMappingURL=app.6b0c407aef849210f220.js.map
--- server/app.js
+++ server/app.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('../webpack.config');
+const path = require('path');
 const PORT = process.env.PORT || 2000;
 // const routes = require('./api/index');
 
 app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.listen(PORT, ()=>{
+if(process.env.NODE_ENV !== 'production'){
+  const webpack = require('webpack');
+  const config = require('../webpack.config');
+  const complier = webpack(config);
+
+  app.use(require('webpack-dev-middleware')(complier, {
+    publicPath: config.output.publicPath,
+    stats: {colors: true}
+  }));
+
+  app.use(require('webpack-hot-middleware')(complier));  
+}
+
+app.use(express.static(path.resolve(__dirname, 'build')))
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, '/build/index.html'))
+})
+
+app.listen(PORT, (err)=>{
+  if(err) console.log(err);
   console.log(`Listening to port ${PORT}`);
 });
