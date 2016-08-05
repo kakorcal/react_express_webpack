64de40eaba16fc18c997fae1203ed44e59235148
Kenneth Korcal <kakorcal@gmail.com>
2016-08-05 16:43:30 -0700

    mock data api now in db

--- server/db/seeds/CREATE_USERS_SEED.js
+++ server/db/seeds/CREATE_USERS_SEED.js
@@ -1,7 +1,17 @@
 exports.seed = function(knex, Promise) {
-  const mock = require('./SEED.js');
+  const users = require('../MOCK_DATA.js');
   return knex('users').del()
     .then(()=>{
-
+      return Promise.all(users.map(user=>{
+        return knex('users').insert({
+          full_name: user.full_name,
+          gender: user.gender,
+          avatar: user.avatar,
+          email: user.email,
+          company: user.company,
+          job_title: user.job_title,
+          description: user.description
+        });
+      }));
     });
 };
