146c15b8159fdcafe21d05f4615950e99df160e6
Kenneth Korcal <kakorcal@gmail.com>
2016-08-05 17:20:42 -0700

    catching errors

--- server/api/users.js
+++ server/api/users.js
@@ -3,33 +3,53 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', (req, res)=>{
-  knex('users').then(users=>{
-    res.send(users);
-  });
+  knex.select(['u.full_name', 'u.gender', 'u.avatar', 'u.company']).from('users as u')
+    .then(users=>{
+      res.send(users);
+    })
+    .catch(err=>{
+      res.send(err);
+    });
 });
 
 router.post('/', (req, res)=>{
-  knex('users').insert(req.body.user).then(()=>{
-    
-  });
+  knex('users').insert(req.body.user)
+    .then(()=>{
+      res.send('User Created');
+    })
+    .catch(err=>{
+      res.send(err);
+    });
 });
 
 router.get('/:id', (req, res)=>{
-  knex('users').where('id', +req.params.id).then(user=>{
-    res.send(user);
-  });
+  knex('users').where('id', +req.params.id)
+    .then(user=>{
+      res.send(user);
+    })
+    .catch(err=>{
+      res.send(err);
+    });
 });
 
 router.put('/:id', (req, res)=>{
-  knex('users').where('id', +req.params.id).update(req.body.user).then(()=>{
-
-  });
+  knex('users').where('id', +req.params.id).update(req.body.user)
+    .then(()=>{
+      res.send('User Updated');
+    })
+    .catch(err=>{
+      res.send(err);
+    });
 });
 
 router.delete('/:id', (req, res)=>{
-  knex('users').where('id', +req.params.id).del().then(()=>{
-
-  });
+  knex('users').where('id', +req.params.id).del()
+    .then(()=>{
+      res.send('User Deleted');
+    })
+    .catch(err=>{
+      res.send(err);
+    });
 });
 
 module.exports = router;
